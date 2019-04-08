d9a6689a6fc96008f80d1b40cb4d08cda8226090
Ygrno <yakirgre@post.bgu.ac.il>
2019-04-09 00:29:00 +0300

    Add project files.

---
+++ Version1U/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Version1U
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
